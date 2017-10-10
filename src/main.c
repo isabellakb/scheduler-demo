11fd6cb97c3e0149428b8206f0c8fd26edc3ce97
isabellakb <32043174+isabellakb@users.noreply.github.com>
2017-10-09 22:00:03 -0300

    Update main.c
    Foram adicionadas as linhas de código contidas dentro do loop for (int i=0;i<n;i++).

--- src/main.c
+++ src/main.c
@@ -6,9 +6,59 @@ int main() {
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
-  }
+int c0=0, c1=0, c2=0, c3=0, c4=0;
+int count;
 
-  return 0;
+for (int i=0;i<n;i++){
+count = 0;
+c0++;
+c1++;
+c2++;
+c3++;
+c4++;
+
+if (c0==a[0]){
+    printf ("0");
+    c0=0;
+}
+else
+    count ++;
+
+if (c1==a[1]){
+    printf ("1");
+    c1=0;
+}
+else
+    count ++;
+
+if (c2==a[2]){
+    printf ("2");
+    c2=0;
+}
+else
+    count ++;
+
+if (c3==a[3]){
+    printf ("3");
+    c3=0;
+}
+else
+    count ++;
+
+if (c4==a[4]){
+    printf ("4");
+    c4=0;
+}
+else
+    count ++;
+
+if (count == 5){
+    printf ("-");
+    printf("\n");
+}
+else
+    printf("\n");
+}
+
+return 0;
 }
